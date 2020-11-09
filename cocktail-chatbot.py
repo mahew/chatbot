f92c66f54646e1673dbbbed91ad4bc6afd7ec2e2
Matt <N0743797@my.ntu.ac.uk>
2020-11-09 13:58:08 +0000

    try catch addition and greetings

--- cocktail-chatbot.py
+++ cocktail-chatbot.py
@@ -1,28 +1,32 @@
 ﻿#!/usr/bin/env python3
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-from sklearn.feature_extraction.text import CountVectorizer
 from PIL import Image
+import matplotlib.pyplot as plt
 import json, requests
 import aiml
-import matplotlib.pyplot as plt
+import sys
 import csv
 import warnings
 
+# base url for the cocktail api
 URL_BASE = "https://www.thecocktaildb.com/api/json/v1/1/"
 
+# read the csv file as a dict object
 CSV = {}
 with open('cocktailQA.csv') as f:
     reader = csv.reader(f, skipinitialspace=True)
     CSV = dict(reader)
 
+# api function to provide information from www.thecocktaildb.com 
 def api(req, search = ""):
+  try:
     if req == "define":
         define_url = r"search.php?s=" + search
         response = get_json_response(define_url)
         if response != False:
-            print("Here is a {}!".format(search))
             show_cocktail(response)
+            print("Here is a {}!".format(search))
             return "Anything else you would like to know?"
             
     elif req == "recipe":
@@ -43,7 +47,7 @@ def api(req, search = ""):
         glass_url = r"search.php?s=" + search
         response = get_json_response(glass_url)
         if response != False:
-
+            # lists of all ingredients           
             ingredients = []
             ingredients.append(response["drinks"][0]["strIngredient1"])
             ingredients.append(response["drinks"][0]["strIngredient2"])
@@ -53,7 +57,7 @@ def api(req, search = ""):
             ingredients.append(response["drinks"][0]["strIngredient6"])
             ingredients.append(response["drinks"][0]["strIngredient7"])
             ingredients.append(response["drinks"][0]["strIngredient8"])
-
+            # lists of all ingredients measures
             ingredient_measures = []
             ingredient_measures.append(response["drinks"][0]["strMeasure1"])
             ingredient_measures.append(response["drinks"][0]["strMeasure2"])
@@ -63,14 +67,16 @@ def api(req, search = ""):
             ingredient_measures.append(response["drinks"][0]["strMeasure6"])
             ingredient_measures.append(response["drinks"][0]["strMeasure7"])
             ingredient_measures.append(response["drinks"][0]["strMeasure8"])
-
+            # iterate the ingredients
             for x in range(8):
+              # if ingredient is present, print it
               if ingredients[x] != None:
+                # some ingredient have a measure, some are un measured, if a measure is present
+                # print it
                 if ingredient_measures[x] != None:
                   print(ingredient_measures[x] + ingredients[x])
                 else:
                   print(ingredients[x])
-
             return "Anything else you would like to know?"
 
     elif req == "random":
@@ -81,8 +87,15 @@ def api(req, search = ""):
             show_cocktail(response)
             return "Anything else you would like to know?"
 
+    elif req == "exit":
+        print("Goodbye! Have a good day!")
+        sys.exit()
+
     else:
         return "I don't understand what you mean! Sorry!"
+
+  except Exception:
+    print("I'm not sure what that is! Try asking me again")
 
 def get_json_response(url):
     response = requests.get(URL_BASE + url)
@@ -96,30 +109,30 @@ def get_json_response(url):
          return False
 
 def show_cocktail(cocktail_json):
+    # get image of drink from api and use matplotlib to show it
     response = requests.get(cocktail_json["drinks"][0]["strDrinkThumb"], stream=True)
     img = Image.open(response.raw)
     plt.imshow(img)
     plt.show()
 
-def get_cosine_sim(*strs): 
-    vectors = [t for t in get_vectors(*strs)]
+def calculate_cosine_simularity(*strings): 
+    vectors = [t for t in get_string_vectors(*strings)]
     # indexing the [0][1] gets us the single cosine simularity between the sentences
     # as calculating the differences produces a 2x2 matrix
     return cosine_similarity(vectors)[0][1]
     
-def get_vectors(*strs):
-    text = [t for t in strs]
-    vectorizer = CountVectorizer(text)
+def get_string_vectors(*strings):
+    text = [t for t in strings]
+    vectorizer = TfidfVectorizer(text)
     vectorizer.fit(text)
     return(vectorizer.transform(text).toarray())
 
 kern = aiml.Kernel()
 kern.setTextEncoding(None)
 kern.bootstrap(learnFiles="cocktail-chatbot.xml")
-vec = TfidfVectorizer(stop_words = 'english')
-# If kern response is defaulted, we can use cosine simularity to check if the inputted command
-# Is similar enough to another command, and then re run it through the aiml agent
-print("Welcome to the cocktail chat bot! Feel free to ask me about cocktails :)")
+
+print("Welcome to the cocktail chat bot! Feel free to ask me about cocktails!")
+
 while True:
     try:
         userInput = input("> ")
@@ -129,10 +142,7 @@ while True:
         break
 
     #pre-process user input and determine response agent (if needed)
-    responseAgent = 'aiml'
-
-    if responseAgent == 'aiml':
-        answer = kern.respond(userInput)
+    answer = kern.respond(userInput)
 
     #post-process the answer for commands
     if answer[0] == '#':
@@ -144,21 +154,28 @@ while True:
 
     # if no aiml command found, use bag of words with tf.idf cosine simularity to respond
     elif answer[0] == '|':
+      try:
         entry = answer[1:]
         # Need to make the string iterable, [] makes string into list
-        highest_simularity = '';
-        highest_simularity_vector = 0;
+        highest_simularity_string = '';
+        highest_simularity = 0;
         for x in CSV:
           with warnings.catch_warnings():
-            # ignore all caught warnings
+            # ignore all caught warnings - commited by filter
             warnings.filterwarnings("ignore")
-            # execute code that will generate warnings
             # For debugging to see all simularities
-            entry_x_similarity = get_cosine_sim(entry, x)
-            if entry_x_similarity > highest_simularity_vector:
-              highest_simularity_vector = entry_x_similarity
-              highest_simularity = x
+            entry_x_similarity = calculate_cosine_simularity(entry, x)
+            if entry_x_similarity > highest_simularity:
+              highest_simularity = entry_x_similarity
+              highest_simularity_string = x
             #print("Vector entry: {} Simularity: {}\n".format(x, entry_x_similarity))
-        print(CSV[highest_simularity])
+        if highest_simularity > 0.5:  
+          print(CSV[highest_simularity_string])
+          #print(highest_simularity)
+        else:
+          print("I'm not quite sure what you mean! Try asking something else!")
+      except Exception:
+        print("I'm not quite sure what you mean! Try asking something else!")
+
     else:
         print(answer)
