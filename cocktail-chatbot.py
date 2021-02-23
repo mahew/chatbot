66a644d03bb550916aab02b7718317a43d691dd5
Matthew <N0743797@my.ntu.ac.uk>
2021-02-23 12:16:18 +0000

    Merge files and add fuzzy and fol elements

--- cocktail-chatbot.py
+++ cocktail-chatbot.py
@@ -1,108 +1,251 @@
-﻿#!/usr/bin/env python3
-from sklearn.feature_extraction.text import TfidfVectorizer
+﻿from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from PIL import Image
-import matplotlib.pyplot as plt
+from nltk.sem import Expression
+from nltk.inference import ResolutionProver
+import tkinter as tk
+from tkinter import filedialog
+import tensorflow as tf
+import tensorflow.keras as keras
+import numpy as np
+#import matplotlib.pyplot as plt
 import json, requests
+import pandas
 import aiml
 import sys
 import csv
 import warnings
+from simpful import *
 
+##############################################################################
+FS = FuzzySystem()
+
+LowT = TriangleFuzzySet(0,7,14,   term="low")
+MedT = TriangleFuzzySet(10,12,40,  term="medium")
+HighT = TriangleFuzzySet(14,40,100, term="high")
+FS.add_linguistic_variable("alcohol", LinguisticVariable([LowT, MedT, HighT], 
+                 concept="Alcohol Strength %", universe_of_discourse=[0,100]))
+
+
+TLV = AutoTriangle(3, terms=['low', 'medium', 'high'], universe_of_discourse=[0,100])
+FS.add_linguistic_variable("sweetness", TLV)
+
+SAT = AutoTriangle(3, terms=['harsh', 'average', 'smooth'], universe_of_discourse=[0,100])
+FS.add_linguistic_variable("smoothness", SAT)
+
+FS.add_rules([
+	"IF (alcohol IS high) OR (sweetness IS low) THEN (smoothness IS harsh)",
+	"IF (alcohol IS low) THEN (smoothness IS smooth)",
+    "IF (alcohol IS medium) AND (sweetness IS low) THEN (smoothness IS average)"
+	"IF (alcohol IS medium) AND (sweetness IS high) THEN (smoothness IS high)"
+	])
+
+#FS.produce_figure(outputfile='plot.pdf')
+##############################################################################
+
+##############################################################################
+img_height = 256
+img_width = 256
+cnn_model = tf.keras.models.load_model('chatbot_model.h5')
+class_names = ['beer', 'cocktail', 'wine']
+##############################################################################
+
+##############################################################################
+# expression for NLP, knowledge base intialisation
+read_expr = Expression.fromstring
+kb=[]
+data = pandas.read_csv('kb.csv', header=None)
+[kb.append(read_expr(row)) for row in data[0]]
+#If we enter a blank expression, the KB will check for contradictions, 
+#returning true if there is a problem
+answer=ResolutionProver().prove("", kb, verbose=True)
+if answer:
+   print("Contradiction with the knowledge base, exiting")
+   sys.exit()
+##############################################################################
+
+##############################################################################
 # base url for the cocktail api
 URL_BASE = "https://www.thecocktaildb.com/api/json/v1/1/"
+##############################################################################
 
+##############################################################################
 # read the csv file as a dict object
 CSV = {}
 with open('cocktailQA.csv') as f:
     reader = csv.reader(f, skipinitialspace=True)
     CSV = dict(reader)
+##############################################################################
+
 
 # api function to provide information from www.thecocktaildb.com 
 def api(req, search = ""):
-  try:
-    if req == "define":
-        # search the api for the given cocktail
-        define_url = r"search.php?s=" + search
-        response = get_json_response(define_url)
-        if response != False:
-            # show the cocktail image if a reponse if given
-            show_cocktail(response)
-            print("Here is a {}!".format(search))
-            return "Anything else you would like to know?"
+    try:
+        if req == "cnn":
+            print("Hmmmm.... let me take a look at it, can you provide the file?")
+            root = tk.Tk()
+            root.withdraw()        
+            file_path = filedialog.askopenfilename()
+            root.lift()
             
-    elif req == "recipe":
-        # gets the recipe for the given cocktail
-        recipe_url = r"search.php?s=" + search
-        response = get_json_response(recipe_url)
-        if response != False:
-            print(response["drinks"][0]["strInstructions"])
+            img = keras.preprocessing.image.load_img(
+                file_path, target_size=(img_height, img_width)
+            )
+            
+            img_array = keras.preprocessing.image.img_to_array(img)
+            img_array = tf.expand_dims(img_array, 0)
+            
+            predictions = cnn_model.predict(img_array)
+            score = tf.nn.softmax(predictions[0])
+            confidence = 100 * np.max(score)
+            
+            if confidence < 70:
+                print("I'm not very sure what this is, sorry! Try ask me what something else is")
+            else:
+                print(
+                    "This image is most likely a {} I have {:.2f} percent confidence in my predicion."
+                    .format(class_names[np.argmax(score)], confidence)
+                )
+            
             return "Anything else you would like to know?"
-
-    elif req == "glass":
-        # gets the glass for the given cocktail
-        glass_url = r"search.php?s=" + search
-        response = get_json_response(glass_url)
-        if response != False:
-            print(response["drinks"][0]["strGlass"])
-            return "Anything else you would like to know?"
-
-    elif req == "ingredients":
-        ingredients_url = r"search.php?s=" + search
-        response = get_json_response(ingredients_url)
-        if response != False:
-            # lists of all ingredients           
-            ingredients = []
-            ingredients.append(response["drinks"][0]["strIngredient1"])
-            ingredients.append(response["drinks"][0]["strIngredient2"])
-            ingredients.append(response["drinks"][0]["strIngredient3"])
-            ingredients.append(response["drinks"][0]["strIngredient4"])
-            ingredients.append(response["drinks"][0]["strIngredient5"])
-            ingredients.append(response["drinks"][0]["strIngredient6"])
-            ingredients.append(response["drinks"][0]["strIngredient7"])
-            ingredients.append(response["drinks"][0]["strIngredient8"])
-            # lists of all ingredients measures
-            ingredient_measures = []
-            ingredient_measures.append(response["drinks"][0]["strMeasure1"])
-            ingredient_measures.append(response["drinks"][0]["strMeasure2"])
-            ingredient_measures.append(response["drinks"][0]["strMeasure3"])
-            ingredient_measures.append(response["drinks"][0]["strMeasure4"])
-            ingredient_measures.append(response["drinks"][0]["strMeasure5"])
-            ingredient_measures.append(response["drinks"][0]["strMeasure6"])
-            ingredient_measures.append(response["drinks"][0]["strMeasure7"])
-            ingredient_measures.append(response["drinks"][0]["strMeasure8"])
-            # iterate the ingredients
-            for x in range(8):
-              # if ingredient is present, print it
-              if ingredients[x] != None:
-                # some ingredient have a measure, some are un measured, if a measure is present
-                # print it
-                if ingredient_measures[x] != None:
-                  print(ingredient_measures[x] + ingredients[x])
+    
+        elif req == "define":
+            # search the api for the given cocktail
+            define_url = r"search.php?s=" + search
+            response = get_json_response(define_url)
+            if response != False:
+                # show the cocktail image if a reponse if given
+                show_cocktail(response)
+                print("Here is a {}!".format(search))
+                return "Anything else you would like to know?"
+                
+        elif req == "recipe":
+            # gets the recipe for the given cocktail
+            recipe_url = r"search.php?s=" + search
+            response = get_json_response(recipe_url)
+            if response != False:
+                print(response["drinks"][0]["strInstructions"])
+                return "Anything else you would like to know?"
+    
+        elif req == "glass":
+            # gets the glass for the given cocktail
+            glass_url = r"search.php?s=" + search
+            response = get_json_response(glass_url)
+            if response != False:
+                print(response["drinks"][0]["strGlass"])
+                return "Anything else you would like to know?"
+    
+        elif req == "ingredients":
+            ingredients_url = r"search.php?s=" + search
+            response = get_json_response(ingredients_url)
+            if response != False:
+                # lists of all ingredients           
+                ingredients = []
+                ingredients.append(response["drinks"][0]["strIngredient1"])
+                ingredients.append(response["drinks"][0]["strIngredient2"])
+                ingredients.append(response["drinks"][0]["strIngredient3"])
+                ingredients.append(response["drinks"][0]["strIngredient4"])
+                ingredients.append(response["drinks"][0]["strIngredient5"])
+                ingredients.append(response["drinks"][0]["strIngredient6"])
+                ingredients.append(response["drinks"][0]["strIngredient7"])
+                ingredients.append(response["drinks"][0]["strIngredient8"])
+                # lists of all ingredients measures
+                ingredient_measures = []
+                ingredient_measures.append(response["drinks"][0]["strMeasure1"])
+                ingredient_measures.append(response["drinks"][0]["strMeasure2"])
+                ingredient_measures.append(response["drinks"][0]["strMeasure3"])
+                ingredient_measures.append(response["drinks"][0]["strMeasure4"])
+                ingredient_measures.append(response["drinks"][0]["strMeasure5"])
+                ingredient_measures.append(response["drinks"][0]["strMeasure6"])
+                ingredient_measures.append(response["drinks"][0]["strMeasure7"])
+                ingredient_measures.append(response["drinks"][0]["strMeasure8"])
+                # iterate the ingredients
+                for x in range(8):
+                  # if ingredient is present, print it
+                  if ingredients[x] != None:
+                    # some ingredient have a measure, some are un measured, if a measure is present
+                    # print it
+                    if ingredient_measures[x] != None:
+                      print(ingredient_measures[x] + ingredients[x])
+                    else:
+                      print(ingredients[x])
+                return "Anything else you would like to know?"
+    
+        elif req == "random":
+            # use random api for to get random cocktail
+            rand_url = r"random.php"
+            response = get_json_response(rand_url)
+            if response != False:
+                print("I reccomend a " + response["drinks"][0]["strDrink"] + " , Here is a picture!")
+                show_cocktail(response)
+                return "Anything else you would like to know?"
+            
+        elif req == "fuzzy":
+            try:
+                alchol_input = int(input("Enter the rough Alcohol % of " + search + " [0-100]> "))
+                sweetness_input = int(input("Enter the rough sweetness of " + search + " [0-100]> "))
+                FS.set_variable("alcohol", alchol_input) 
+                FS.set_variable("sweetness", sweetness_input) 
+                smoothness = FS.inference()
+                print(smoothness)
+                if smoothness["smoothness"] < 33:
+                    print("The " + search + " might be quite harsh! Drink carefully")
+                elif smoothness["smoothness"] < 66:
+                    print("The " + search + " will be alright, but might tingle")
                 else:
-                  print(ingredients[x])
+                    print("The " + search + " will be very smooth, enjoy!")
+            except Exception as e:
+                print("Sorry, you provided an incorrect input, please try asking again")
             return "Anything else you would like to know?"
-
-    elif req == "random":
-        # use random api for to get random cocktail
-        rand_url = r"random.php"
-        response = get_json_response(rand_url)
-        if response != False:
-            print("I reccomend a " + response["drinks"][0]["strDrink"] + " , Here is a picture!")
-            show_cocktail(response)
+    
+        elif req == "knowledge":
+            object, subject = search.split(' is ')
+            expr=read_expr(subject + '(' + object + ')')
+            kb.append(expr) 
+            answer=ResolutionProver().prove(expr, kb, verbose=True)
+            if answer:
+                kb.remove(expr)
+                print("This is contradicting! I have ignored you.")
+            else:
+                print('OK, I will remember that',object,'is', subject)
             return "Anything else you would like to know?"
+    
+        elif req == "checkknowledge": 
+            # check that * is *"
+            object, subject = search.split(' is ')
+            print(subject)
+            raw_expr = subject + '(' + object + ')'
+            expr=read_expr(raw_expr)
+            answer=ResolutionProver().prove(expr, kb, verbose=True)
+            if answer:
+               return "That is Correct! Anything else you would like to know?"
+            else:
+               if "not" in subject:
+                   subject = subject.replace("not", "")
+               else:
+                   subject = "not " + subject
+               expr=read_expr(subject + '(' + object + ')')
+               print(expr)
+               answer=ResolutionProver().prove(expr, kb, verbose=True)
+               if answer:
+                   print("This is definitely false")
+               else:
+                   print("Sorry I don't know.")
+               return "Anything else you would like to know?"
 
-    elif req == "exit":
-        # exit the program
-        print("Goodbye! Have a good day!")
-        sys.exit()
+        elif req == "exit":
+            # exit the program
+            print("Goodbye! Have a good day!")
+            sys.exit()
 
-    # if not found, print a message
-    else:
-        return "I don't understand what you mean! Sorry!"
+        # if not found, print a message
+        else:
+            return "I don't understand what you mean! Sorry!"
 
-  except Exception:
-    print("I'm not sure what that is! Try asking me again")
+    except Exception as e:
+      print(e)
+      print("I'm not sure what that is! Try asking me again")
+    
 
 def get_json_response(url):
     # use base url and additional url arguments provided
@@ -121,8 +264,8 @@ def show_cocktail(cocktail_json):
     # get image of drink from api and use matplotlib to show it
     response = requests.get(cocktail_json["drinks"][0]["strDrinkThumb"], stream=True)
     img = Image.open(response.raw)
-    plt.imshow(img)
-    plt.show()
+    #plt.imshow(img)
+    #plt.show()
 
 def calculate_cosine_simularity(*strings): 
     vectors = [t for t in get_string_vectors(*strings)]
--- train-cnn.py
+++ train-cnn.py
@@ -1,41 +1,101 @@
 import tensorflow as tf
-from tensorflow.keras import datasets, layers, models
+from tensorflow.keras import layers
 import matplotlib.pyplot as plt
 
-wget https://www.kaggle.com/khalilnouisser/alcoolicdrinks5/download
+#Using https://www.tensorflow.org/tutorials/images/cnn as a guide
 
-(train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
+batch_size = 64
+img_height = 256
+img_width = 256
 
-# Normalize pixel values to be between 0 and 1
-train_images, test_images = train_images / 255.0, test_images / 255.0
+data_dir = "train"
 
-class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
-               'dog', 'frog', 'horse', 'ship', 'truck']
+train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    data_dir,
+    validation_split=0.2,
+    subset="training",
+    seed=123,
+    image_size=(img_height, img_width),
+    batch_size=batch_size)
 
-model = models.Sequential()
-model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-model.add(layers.Flatten())
-model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dense(10))
+val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="validation",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
 
-model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['accuracy'])
+class_names = train_ds.class_names
+normalization_layer = layers.experimental.preprocessing.Rescaling(1./255)
+normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+image_batch, labels_batch = next(iter(normalized_ds))
 
-history = model.fit(train_images, train_labels, epochs=10, 
-                    validation_data=(test_images, test_labels))
+num_classes = 3
 
-plt.plot(history.history['accuracy'], label='accuracy')
-plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
-plt.xlabel('Epoch')
-plt.ylabel('Accuracy')
-plt.ylim([0.5, 1])
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+data_augmentation = tf.keras.Sequential(
+  [
+    layers.experimental.preprocessing.RandomFlip("horizontal", 
+                                                 input_shape=(img_height, 
+                                                              img_width,
+                                                              3)),
+    layers.experimental.preprocessing.RandomRotation(0.1),
+    layers.experimental.preprocessing.RandomZoom(0.1),
+  ]
+)
+
+model = tf.keras.Sequential([
+  data_augmentation,
+  layers.experimental.preprocessing.Rescaling(1./255),
+  layers.Conv2D(16, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(32, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(64, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Dropout(0.4),
+  layers.Flatten(),
+  layers.Dense(128, activation='relu'),
+  layers.Dense(num_classes)
+])
+
+model.compile(
+  optimizer='adam',
+  loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True),
+  metrics=['accuracy'])
+
+epochs = 15
+
+history = model.fit(train_ds,
+                    validation_data=val_ds,
+                    epochs = epochs) 
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(epochs)
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
 plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
 
-test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
 
-print(test_acc)
+#https://www.tensorflow.org/tutorials/keras/save_and_load
+
+model.save('chatbot_model.h5')
