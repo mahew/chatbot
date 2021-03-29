6d12fdefe59a3d84aec3de31a097c36ddcea808c
Matthew <matt@matthew-ball.com>
2021-03-29 13:42:55 +0100

    Update cocktail-chatbot.py

--- cocktail-chatbot.py
+++ cocktail-chatbot.py
@@ -18,6 +18,12 @@ import warnings
 from simpful import *
 
 ##############################################################################
+cog_key = 'e3f58c2d955747099f59135d793e2ce8'
+cog_endpoint = 'https://mattschatbotservice.cognitiveservices.azure.com/'
+cog_region = 'westeurope'
+##############################################################################
+
+##############################################################################
 FS = FuzzySystem()
 
 LowT = TriangleFuzzySet(4,7,14,   term="low")
